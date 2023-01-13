9bf4002dd6f8546e08e11cb9975f02bdbdad3812
Jason Stewart <jason.e.stewart@gmail.com>
2023-01-12 20:56:40 -0700

    react functional

--- web-frontend/src/components/Words.tsx
+++ web-frontend/src/components/Words.tsx
@@ -1,38 +1,34 @@
-import * as React from 'react';
-import GhotiModel from '../model/GhotiModel';
-import Word from './Word';
+import React from "react";
+import GhotiModel from "../model/GhotiModel";
+import Word from "./Word";
 
 type MyProps = {
-    model: GhotiModel,
-    finished: boolean
-}
+    model: GhotiModel;
+    finished: boolean;
+};
 
-// type MyState = {
-//     wordLayout: React.ReactNode,
-// }
-
-class Words extends React.Component<MyProps, {}> {    
-    makeWordLayout() {
+const Words = ({ model, finished }: MyProps) => {
+    const makeWordLayout = () => {
         // store the <ul>s in an array before the return
-		const completed: React.ReactNode[] = [];
-        const wordList: string[] = this.props.model.getCurrentWordList();
+        const completed: React.ReactNode[] = [];
+        const wordList: string[] = model.getCurrentWordList() ?? [];
 
         // order the words by size
-		var orderedWords: string[][] = [];
-		for (let i=3; i <= 7; i++) {
-			orderedWords[i] = [];
-		}
-		wordList.forEach((word) => {
-			orderedWords[word.length].push(word.toUpperCase());
-		});
-        
+        var orderedWords: string[][] = [];
+        for (let i = 3; i <= 7; i++) {
+            orderedWords[i] = [];
+        }
+        wordList.forEach((word) => {
+            orderedWords[word.length].push(word.toUpperCase());
+        });
+
         // now that they're ordered, make columns of words
         orderedWords.forEach((array) => {
             // store the <li>s in an array before creating the <ul>
             let container: React.ReactNode[] = [];
 
-			let count = 0;
-			array.forEach((word) => {
+            let count = 0;
+            array.forEach((word) => {
                 count++;
 
                 if (count > 12) {
@@ -41,19 +37,20 @@ class Words extends React.Component<MyProps, {}> {
                     count = 0;
                 }
                 // store the word for later rendering
-                container.push(<Word finished={this.props.finished} word={word} key={word} hidden={!this.props.model.isAlreadyGuessed(word)} />);
-			});
+                container.push(
+                    <Word
+                        finished={finished}
+                        word={word}
+                        key={word}
+                        hidden={!model.isAlreadyGuessed(word)}
+                    />
+                );
+            });
             completed.push(<ul>{container}</ul>);
-		});
-        return <div>{completed}</div>
-    }
-    render() {
-        return (
-            <div id="completed">
-                {this.makeWordLayout()}
-            </div>
-        );
-    }
+        });
+        return <div>{completed}</div>;
+    };
+    return <div id="completed">{makeWordLayout()}</div>;
 };
 
 export default Words;
