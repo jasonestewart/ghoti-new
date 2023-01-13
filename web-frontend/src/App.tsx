b4793ead590a68b76da891cf42b7a1dc4356f43d
Jason Stewart <jason.e.stewart@gmail.com>
2023-01-12 20:58:12 -0700

    react functional

--- web-frontend/src/App.tsx
+++ web-frontend/src/App.tsx
@@ -1,39 +1,23 @@
-import * as React from 'react';
-import Header from './components/Header';
-import './App.css';
+import Header from "./components/Header";
+import "./App.css";
+import GhotiModel from "./model/GhotiModel";
 
-import GhotiModel from './model/GhotiModel';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Ghoti from "./components/Ghoti";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import Ghoti from './components/Ghoti';
-
-type GhotiState = {
-  started: boolean
+const App = () => {
+    const model = new GhotiModel();
+    return (
+        <>
+            <div id="home_wrap">
+                <Header />
+                <Ghoti model={model} />
+            </div>
+        </>
+    );
 };
 
-class App extends React.Component<{}, GhotiState> {
-  model: GhotiModel;
-
-  state = {
-    started: false
-  };
-
-  constructor() {
-    super({});
-    this.model = new GhotiModel();
-  }
-  
-  render() {
-    return (
-      <div id="home_wrap">
-        <Header />
-        <Ghoti model={this.model} />
-      </div>
-    );
-
-  }
-}
 export default App;
