2a4f4aff27237a64b00912614dc635a3c596a7f3
Jason Stewart <jason.e.stewart@gmail.com>
2023-01-12 20:56:03 -0700

    helpers

---
+++ web-frontend/src/util/helper.ts
@@ -0,0 +1,12 @@
+export const playBadSound = () => {
+  const badSound = new Audio("/badSound.mp3");
+  badSound.play();
+};
+export const playOldSound = () => {
+  const oldSound = new Audio("/oldSound.mp3");
+  oldSound.play();
+};
+export const playGoodSound = () => {
+  const goodSound = new Audio("/goodSound.mp3");
+  goodSound.play();
+};
