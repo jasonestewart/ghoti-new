b64ccfc47925bf203d173435e33336d569da6ba6
Jason Stewart <jason.e.stewart@gmail.com>
2023-01-12 21:02:40 -0700

    react functional

--- web-frontend/src/components/Game.tsx
+++ web-frontend/src/components/Game.tsx
@@ -1,274 +1,191 @@
-import * as React from 'react';
-import ScoreCard from './ScoreCard';
-import Words from './Words';
-import GhotiModel, { Guess } from '../model/GhotiModel';
-import Message from './Message';
+import { useEffect, useState } from "react";
+import ScoreCard from "./ScoreCard";
+import Words from "./Words";
+import GhotiModel, { Guess } from "../model/GhotiModel";
+import Message from "./Message";
+import { playBadSound, playGoodSound, playOldSound } from "../util/helper";
+
+const GAME_TIME = 120;
+let timerCreated = false;
 
 type MyProps = {
-    model: GhotiModel
-}
+    model: GhotiModel;
+};
+const Game = ({ model }: MyProps) => {
+    const EMPTY = "";
+    const [paused, setPaused] = useState(false);
+    const [finished, setFinished] = useState(false);
+    const [message, setMessage] = useState("");
+    const [timeStr, setTimeStr] = useState("");
+    const [lastLetter, setLastLetter] = useState("");
+    const [count, setCount] = useState(GAME_TIME);
 
-type MyState = {
-    paused: boolean,
-    finished: boolean,
-    lastLetter: string,
-    timeStr: string,
-    message: string
-}
+    useEffect(() => {
+        if (count > 1) {
+            if (!finished) {
+                if (paused) {
+                    setTimeStr("PAUSED");
+                    console.log("timer is paused");
+                } else {
+                    const minutes = Math.floor(count / 60);
+                    const seconds = count - minutes * 60;
+                    const formatSeconds =
+                        seconds < 10 ? "0" + seconds : +seconds;
 
-class Game extends React.Component<MyProps, MyState> {
-    EMPTY = '';
-    GAME_TIME = 120;
-    goodSound: HTMLAudioElement;
-    badSound: HTMLAudioElement;
-    oldSound: HTMLAudioElement;
+                    setTimeStr(`${minutes} : ${formatSeconds}`);
 
-    constructor(props: MyProps) {
-        super(props);
-        this.state = {
-            paused: false,
-            finished: false,
-            message: '',
-            timeStr: '',
-            lastLetter: ''
-        };
-        this.CreateTimer();
+                    const timer = setTimeout(
+                        () => !paused && setCount(count - 1),
+                        1e3
+                    );
 
-        this.goodSound = new Audio('/goodSound.mp3');
-        this.badSound = new Audio('/badSound.mp3');
-        this.oldSound = new Audio('/oldSound.mp3');
-    }
-	
-    render() {
-        const paused = this.state.paused ? <span>Your Game is paused</span> : null;
-        let newRound = <span></span>;
-        if (this.state.finished) {
-            if (this.props.model.isSuccess()) {
-                newRound = <button onClick={this.nextRound}>Proceed</button>;
+                    console.log("UpdateTimer: calling for secs: ", count);
+                    return () => clearTimeout(timer);
+                }
+            }
+        } else {
+            let msg = "";
+            if (model.isSuccess()) {
+                msg =
+                    "Congrats, you've made it to round " +
+                    (model.getRound() + 1);
             } else {
-                newRound = <button onClick={this.restart}>Restart</button>;
+                msg = "Too bad, Game over!";
             }
+
+            setPaused(true);
+            setFinished(true);
+            setMessage(msg);
         }
+    }, [count, paused, finished, model]);
 
-        return (
-            <div className='clearfix'>
-                <div className="" id="timer">{this.state.timeStr}</div>
-                {paused}
-                <Message message={this.state.message} />
-                {newRound}
-                <Words model={this.props.model} finished={this.state.finished} />
-                <br className='clear' />
-                <ScoreCard 
-                    model={this.props.model} />
-            </div>
-        );
-    }
+    const CreateTimer = () => {
+        setCount(GAME_TIME);
+        console.log("Creating timer");
+    };
 
-    componentDidMount() {
-        // @ts-ignore
-        window.addEventListener("keydown", this.handleKeyPress);
-        console.log("mounting listener");
-      }
-    
-    componentWillUnmount() {
-        // @ts-ignore
-        window.removeEventListener("keydown", this.handleKeyPress);
-        console.log("unmounting listener");
-    }
+    const nextRound = async () => {
+        console.log("Next round");
 
-    restart = () => {
-        this.props.model.restart();
-        this.setState({
-            paused: false,
-            finished: false,
-            message: ''
-        });
+        await model.newRound();
+        setPaused(false);
+        setFinished(false);
+        setMessage("");
 
-        this.CreateTimer();
-    }
-    
-    nextRound = async () => {
-        await this.props.model.newRound();
-        this.setState({
-            paused: false,
-            finished: false,
-            message: ''
-        });
-
-        this.CreateTimer();
-    }
-
-    timeIsUp = () => {
-        let msg = '';
-        if (this.props.model.isSuccess()) {
-            msg = "Congrats, you've made it to round " + (this.props.model.getRound() + 1);
-        } else {
-            msg = "Too bad, Game over!";
-        }
-
-        this.setState({
-            paused: true,
-            finished: true,
-            message: msg
-        });
-    }
-
-    UpdateTimer = (secs: number) => {
-        if (this.state.paused) {
-            this.setState({timeStr: 'PAUSED'});
-            console.log('timer is paused');
-        } else {
-            const minutes = Math.floor(secs / 60);
-            const seconds = secs - (minutes * (60));
-
-            this.setState({timeStr: minutes + ":" + this.LeadingZero(seconds)});
-        }
-
-        console.log("UpdateTimer: calling for secs: ", secs);
-    }
-
-
-    LeadingZero = (time: number) => {
-        return (time < 10) ? "0" + time : + time;
-    }
-
-    CreateTimer = () => {
-        this.UpdateTimer(this.GAME_TIME);
-        setTimeout(() => this.Tick(this.GAME_TIME), 1000);
-    }
-
-    Tick = (time: number) => {
-        if (time <= 0) {
-            this.timeIsUp();
-        } else {
-            if (!this.state.paused) {
-                time -= 1;
-            }
-            if (!this.state.finished) {
-                this.UpdateTimer(time);
-                setTimeout(() => this.Tick(time), 1000);
-            }
-        }
-    }
-
-    // backspace removes the previous letter from the guess
-    handleBackspace() {
-        this.props.model.undoPrevGuess();
-    }
-
-    // tab shuffles the letters
-    handleTab() {
-        this.props.model.shuffle();
-    }
-    checkGuess() {
-        const result = this.props.model.makeGuess();
-        if (result === Guess.BAD_GUESS) {
-            this.playBadSound();
-        } else if (result === Guess.ALREADY_GUESSED) {
-                this.playOldSound();
-        } else {
-            this.playGoodSound();
-        }
-    }
-    playBadSound() {
-        this.badSound.play();
-    }
-    playOldSound() {
-        this.oldSound.play();
-    }
-    playGoodSound() {
-        this.goodSound.play();
-    }
-    
+        CreateTimer();
+    };
 
     // enter guesses a word
-    handleEnter() {
-        // Check guess
-        this.checkGuess();
-            
-      if (this.props.model.isFinished()) {
-          this.nextRound();
-      }
+    const handleEnter = () => {
+        const result = model.makeGuess();
+        if (result === Guess.BAD_GUESS) {
+            playBadSound();
+        } else if (result === Guess.ALREADY_GUESSED) {
+            playOldSound();
+        } else {
+            playGoodSound();
+        }
 
-    }
+        if (model.isFinished()) {
+            nextRound();
+        }
+    };
 
     // escape toggles the game paused state
-    handleEsc = () => {
-        this.setState({paused: !this.state.paused});
-        console.log("setting paused: ", this.state.paused);
-    }
+    const handleEsc = () => {
+        console.log("setting paused: ", !paused);
+        setPaused(!paused);
+    };
 
     // delete cheats
-    handleDel = () => {
-        this.setState({message: 'Your word is: '+this.props.model.getCurrentWord()});
-        console.log("setting paused: ", this.state.paused);
-    }
+    const handleDel = () => {
+        setMessage(`Your word is: ${model.getCurrentWord()}`);
+        console.log("Your paused setting is: ", paused);
+    };
+
+    // backspace removes the previous letter from the guess
+    const handleBackspace = () => {
+        model.undoPrevGuess();
+    };
+
+    // tab shuffles the letters
+    const handleTab = () => {
+        model.shuffle();
+    };
 
     // the main key event callback
-    handleKeyPress = (event: React.KeyboardEvent) => {
+    const handleKeyPress = (event: KeyboardEvent) => {
         event.preventDefault();
 
         const key = event.key;
-        this.setState({lastLetter: key});
-        
+        setLastLetter(key);
+
         if (key === "Escape") {
-            this.handleEsc();
+            handleEsc();
         } else if (key === "Delete") {
-            this.handleDel();
-        } else if (!this.state.paused) {
+            handleDel();
+        } else if (!paused) {
             if (key === "Backspace") {
-                this.handleBackspace();
+                handleBackspace();
             } else if (key === "Enter") {
-                this.handleEnter();
+                handleEnter();
             } else if (key === "Tab") {
-                this.handleTab();
+                handleTab();
             } else {
                 const char = key.toUpperCase();
-                if (this.props.model.checkChar(char)) {
-                    this.props.model.wordToGuess(char);
+                if (model.checkChar(char)) {
+                    model.wordToGuess(char);
                 }
             }
         }
+    };
+
+    useEffect(() => {
+        if (!timerCreated) {
+            window.addEventListener("keydown", (e) => handleKeyPress(e));
+            console.log("mounting listener");
+            CreateTimer();
+            timerCreated = true;
+        }
+        return () => {
+            window.removeEventListener("keydown", (e) => handleKeyPress(e));
+            console.log("unmounting listener");
+        };
+    }, []);
+
+    let newRound = <span></span>;
+    if (finished) {
+        if (model.isSuccess()) {
+            newRound = <button onClick={() => nextRound()}>Proceed</button>;
+        } else {
+            newRound = <button onClick={() => restart()}>Restart</button>;
+        }
     }
 
+    const restart = () => {
+        model.restart();
+        setPaused(false);
+        setFinished(false);
+        setMessage("");
 
-    // alert_message(message: string) {
-    //     const fade: HTMLDivElement = document.createElement('div');
-    //     fade.setAttribute('class', 'fade-in');
-    //     fade.style.display = 'block';
+        CreateTimer();
+    };
 
-    //     const mh: HTMLDivElement = document.createElement('div');
-    //     mh.setAttribute('id', 'message-holder');
-    //     mh.style.opacity = '0';
-    //     fade.appendChild(mh);
-
-    //     var p = document.createElement('p');
-    //     p.innerHTML = message;
-    //     mh.appendChild(p);
-    //     document.body.insertBefore(fade, document.getElementById('background'));
-    //     this.anim_in(mh, 0);
-    // }
-    // anim_out = (ele: HTMLDivElement, op: number) => {
-    //     op = op - 0.1;
-    //     ele.style.opacity = op.toString();
-    //     if(op > 0){
-    //         setTimeout(() => {this.anim_out(ele, op)},100);
-    //     } else {
-    //         ele.style.opacity = '0';
-    //     }
-    // }
-    // anim_in = (ele: HTMLDivElement, op: number) => {
-    //     op = op + 0.1;
-    //     ele.style.opacity = op.toString();
-    //     if (op < 1) {
-    //         setTimeout(() => {this.anim_in(ele, op)},100);
-    //     } else {
-    //         ele.style.opacity = '1';
-    //         setTimeout(() => {this.anim_out(ele, op)},10000);
-    //     }	
-    // }
-
-
-    
+    return (
+        <div className="clearfix">
+            <div className="" id="timer">
+                {timeStr}
+            </div>
+            <div>
+                <button onClick={() => setPaused(true)}>pause</button>
+                <button onClick={() => setPaused(false)}>resume</button>
+            </div>
+            <Message message={message} />
+            {newRound}
+            <Words model={model} finished={finished} />
+            <br className="clear" />
+            <ScoreCard model={model} />
+        </div>
+    );
 };
 export default Game;
