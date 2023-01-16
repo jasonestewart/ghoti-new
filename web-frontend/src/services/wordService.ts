0c495b11606388dbe9fff38792af5e67ee5dd2ec
Jason Stewart <jason.e.stewart@gmail.com>
2023-01-16 14:57:08 -0700

    local word service

--- web-frontend/src/services/wordService.ts
+++ web-frontend/src/services/wordService.ts
@@ -1,41 +1,19 @@
-import { GraphQLClient } from "graphql-request";
+import ALLWORDS from "../data/data";
 
 export type Word = {
   text: string;
   words: string[];
 };
 
-export type Query = {
-  // field return type name
-  next: Word;
-};
-
 class WordService {
-  __port = 4000;
-  __host = "http://localhost";
-  __url = `${this.__host}:${this.__port}`;
-  // __url = 'https://ghoti-backend.herokuapp.com/';
-  __client: GraphQLClient;
-  __nextWord = `#graphql
-    query  Query {
-      next {
-        text
-        words
-      }
-    }
-    `;
+  nextWord(): Word {
+    const rndm = Math.floor(Math.random() * ALLWORDS.length);
 
-  constructor() {
-    this.__client = new GraphQLClient(`${this.__url}`);
-  }
-
-  async nextWord(): Promise<Word> {
-    const query: Query = await this.__client.request(this.__nextWord);
-    const word = query.next;
+    const word = ALLWORDS[rndm];
     const words = word.words;
     words.push(word.text);
     return { text: word.text, words: words };
-  }
+};
 }
 
 export default WordService;
