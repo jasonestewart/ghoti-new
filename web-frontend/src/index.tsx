5369836edc1985c34be0db7691fd6d8b67eeace1
Jason Stewart <jason.e.stewart@gmail.com>
2023-01-16 21:43:04 -0700

    make Ghoti the main component

--- web-frontend/src/index.tsx
+++ web-frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Ghoti from './components/Ghoti';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Ghoti />
   </React.StrictMode>
 );
 
