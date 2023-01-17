62226cdd8ed2788e0f2bab1b10673cad856cbf24
Jason Stewart <jason.e.stewart@gmail.com>
2023-01-16 21:44:54 -0700

    create wordService and ghotiModel in useEffect; dialog to start

--- web-frontend/src/components/Ghoti.tsx
+++ web-frontend/src/components/Ghoti.tsx
@@ -1,27 +1,71 @@
 import Game from "./Game";
 import GhotiModel from "../model/GhotiModel";
-import { useState } from "react";
-import StartInfo from "./StartInfo";
+import { useEffect, useState } from "react";
+import { Dialog } from "@headlessui/react";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import Header from "./Header";
+import "../App.css";
+import WordService from "../services/wordService";
 
-type MyProps = {
-    model: GhotiModel;
-};
+let initialized = false;
 
-const Ghoti = ({ model }: MyProps) => {
+const Ghoti = () => {
     const [isOpen, setIsOpen] = useState(true);
+    const [model, setModel] = useState<GhotiModel>();
 
+    useEffect(() => {
+        if (!initialized) {
+            initialized = true;
+            const service = new WordService();
+            setModel(new GhotiModel(service));
+            console.log(`word service has ${service.wordCount} words`);
+        }
+      }, []);
+    
     const start = async () => {
         console.log("starting game");
 
-        await model.restart();
+        model!.restart();
         setIsOpen(false);
     };
 
-    if (isOpen) {
-        return <StartInfo callback={start} />;
-    }
+    return (
+        <>
+            <div id="home_wrap">
+                <Header />
 
-    return <Game model={model} />;
+                <Dialog
+                    open={isOpen}
+                    onClose={() => start()}
+                    className="relative z-50"
+                >
+                    <div className="fixed inset-0 flex bg-black/30 items-center justify-center p-4">
+                        <Dialog.Panel className="w-full max-w-sm rounded bg-slate-500 p-4 text-center">
+                            <Dialog.Title className="text-center">
+                                Start Ghoti
+                            </Dialog.Title>
+                            <Dialog.Description>
+                                This will start the game
+                            </Dialog.Description>
+
+                            <p>Good Luck</p>
+
+                            <button
+                                className="inline-flex items-center rounded border border-transparent bg-indigo-600 px-2.5 py-1.5 text-xs font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                                onClick={() => start()}
+                            >
+                                Start
+                            </button>
+                        </Dialog.Panel>
+                    </div>
+                </Dialog>
+                {!isOpen && model && <Game model={model} />}
+            </div>
+        </>
+    );
 };
 
 export default Ghoti;
