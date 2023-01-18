964fd5cf797c6150ee643e4a639a63898e0120db
Jason Stewart <jason.e.stewart@gmail.com>
2023-01-17 23:02:13 -0700

    fixed letter box height

--- web-frontend/src/components/Letters.tsx
+++ web-frontend/src/components/Letters.tsx
@@ -10,25 +10,25 @@ const Letters = ({ letters, prefix }: MyProps) => {
             className="grid grid-cols-7 gap-4 letter_container"
         >
             <div key={`${prefix}.ltr_1`} className="letter col-span-1">
-                {letters[0]}
+                <div className="">{letters[0]}</div>
             </div>
             <div key={`${prefix}.ltr_2`} className="letter col-span-1">
-                {letters[1]}
+                <div className="">{letters[1]}</div>
             </div>
             <div key={`${prefix}.ltr_3`} className="letter col-span-1">
-                {letters[2]}
+                <div className="">{letters[2]}</div>
             </div>
             <div key={`${prefix}.ltr_4`} className="letter col-span-1">
-                {letters[3]}
+                <div className="">{letters[3]}</div>
             </div>
             <div key={`${prefix}.ltr_5`} className="letter col-span-1">
-                {letters[4]}
+                <div className="">{letters[4]}</div>
             </div>
             <div key={`${prefix}.ltr_6`} className="letter col-span-1">
-                {letters[5]}
+                <div className="">{letters[5]}</div>
             </div>
             <div key={`${prefix}.ltr_7`} className="letter col-span-1">
-                {letters[6]}
+                <div className="">{letters[6]}</div>
             </div>
         </div>
     );
