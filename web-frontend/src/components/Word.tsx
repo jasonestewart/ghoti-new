e543fc356bcaa7d5020bf572d30ba96a87b8e776
Jason Stewart <jason.e.stewart@gmail.com>
2023-01-17 17:26:54 -0700

    ugly grid cols

--- web-frontend/src/components/Word.tsx
+++ web-frontend/src/components/Word.tsx
@@ -9,18 +9,23 @@ export function classNames(...classes: string[]) {
 
 const Word = ({ word, hidden, finished }: MyProps) => {
     const wordLength = word.length;
-    const visibility = finished && hidden ? "red" : "";
+    const visibility =
+        finished && hidden ? "red" : hidden ? "hidden" : "visible";
+    const gridCols =
+        wordLength === 3
+            ? "grid-cols-3"
+            : wordLength === 4
+            ? "grid-cols-4"
+            : wordLength === 5
+            ? "grid-cols-5"
+            : wordLength === 6
+            ? "grid-cols-6"
+            : "grid-cols-7";
     return (
-        <div
-            className={`${visibility} grid grid-cols-${wordLength} gap-1`}
-            style={{
-                gridTemplateColumns: `repeat(${wordLength}, minmax(0,1fr))`,
-            }}
-            id={word}
-        >
+        <div className={classNames("grid", gridCols)} id={word}>
             {word.split("").map((letter, i) => (
-                <div key={`${letter}-${i}`} className="">
-                    {letter}
+                <div key={`${letter}-${i}`} className="letter_box">
+                    <div className={visibility}>{letter}</div>
                 </div>
             ))}
         </div>
