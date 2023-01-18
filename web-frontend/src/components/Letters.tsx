343556ade587e1dfa005d85ce62e5be1e038a677
Jason Stewart <jason.e.stewart@gmail.com>
2023-01-17 17:25:52 -0700

    removed ids

--- web-frontend/src/components/Letters.tsx
+++ web-frontend/src/components/Letters.tsx
@@ -5,54 +5,29 @@ type MyProps = {
 
 const Letters = ({ letters, prefix }: MyProps) => {
     return (
-        <div id="word_container" className="grid grid-cols-7 gap-4 letter_container">
-            <div
-                id={`${prefix}.ltr_1`}
-                key={`${prefix}.ltr_1`}
-                className="letter col-span-1"
-            >
+        <div
+            id="word_container"
+            className="grid grid-cols-7 gap-4 letter_container"
+        >
+            <div key={`${prefix}.ltr_1`} className="letter col-span-1">
                 {letters[0]}
             </div>
-            <div
-                id={`${prefix}.ltr_2`}
-                key={`${prefix}.ltr_2`}
-                className="letter col-span-1"
-            >
+            <div key={`${prefix}.ltr_2`} className="letter col-span-1">
                 {letters[1]}
             </div>
-            <div
-                id={`${prefix}.ltr_3`}
-                key={`${prefix}.ltr_3`}
-                className="letter col-span-1"
-            >
+            <div key={`${prefix}.ltr_3`} className="letter col-span-1">
                 {letters[2]}
             </div>
-            <div
-                id={`${prefix}.ltr_4`}
-                key={`${prefix}.ltr_4`}
-                className="letter col-span-1"
-            >
+            <div key={`${prefix}.ltr_4`} className="letter col-span-1">
                 {letters[3]}
             </div>
-            <div
-                id={`${prefix}.ltr_5`}
-                key={`${prefix}.ltr_5`}
-                className="letter col-span-1"
-            >
+            <div key={`${prefix}.ltr_5`} className="letter col-span-1">
                 {letters[4]}
             </div>
-            <div
-                id={`${prefix}.ltr_6`}
-                key={`${prefix}.ltr_6`}
-                className="letter col-span-1"
-            >
+            <div key={`${prefix}.ltr_6`} className="letter col-span-1">
                 {letters[5]}
             </div>
-            <div
-                id={`${prefix}.ltr_7`}
-                key={`${prefix}.ltr_7`}
-                className="letter col-span-1"
-            >
+            <div key={`${prefix}.ltr_7`} className="letter col-span-1">
                 {letters[6]}
             </div>
         </div>
