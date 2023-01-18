1bdfad27a7f0f32c2ae0df0394785ea08853521b
Jason Stewart <jason.e.stewart@gmail.com>
2023-01-17 17:25:33 -0700

    normal

--- web-frontend/src/index.tsx
+++ web-frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import ReactDOM from "react-dom/client";
-import "./index2.css";
+import "./index.css";
 import Ghoti from "./components/Ghoti";
 
 const root = ReactDOM.createRoot(
